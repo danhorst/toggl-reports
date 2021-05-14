c8641ef5d3b3e6c2d03fb6e81908b5215c3ce24a
Dan Horst <dbrubak1@nd.edu>
2021-05-14 11:32:22 -0400

    Adding remaining required parameters
    Why do they ask for the user agent as a request parameter?
    There's already a header for that :man_facepalming:

--- report.rb
+++ report.rb
@@ -3,4 +3,4 @@
 require 'dotenv'
 Dotenv.load
 
-`curl -v -u #{ENV['TOGGL_API_KEY']}:api_token -H "User-Agent: Weekly-Decimal-Time" -H "Content-Type: application/json" https://api.track.toggl.com/reports/api/v2/weekly -o weekly.json`
+`curl -v -u #{ENV['TOGGL_API_KEY']}:api_token -H "User-Agent: Weekly-Decimal-Time" -H "Content-Type: application/json" "https://api.track.toggl.com/reports/api/v2/weekly?user_agent=Weekly-Decimal-Time&workspace_id=#{ENV['TOGGL_WORKSPACE_ID']}" -o weekly.json`
