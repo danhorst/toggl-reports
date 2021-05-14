da7af88cf55d0be5e7dd88104bc536abd2284720
Dan Horst <dbrubak1@nd.edu>
2021-05-14 14:45:40 -0400

    Adding module namespace

--- report.rb
+++ report.rb
@@ -7,31 +7,38 @@ require 'json'
 require 'dotenv'
 Dotenv.load
 
-def report_uri(
-  user_agent: 'Weekly-Decimal-Time',
-  workspace_id: ENV['TOGGL_WORKSPACE_ID']
-)
-  URI("https://api.track.toggl.com/reports/api/v2/weekly?user_agent=#{user_agent}&workspace_id=#{workspace_id}")
+module WeeklyReport
+  # Returns the weekly report data from the Toggl reports API
+  module Json
+    module_function
+
+    def uri(
+      user_agent: 'Weekly-Decimal-Time',
+      workspace_id: ENV['TOGGL_WORKSPACE_ID']
+    )
+      URI("https://api.track.toggl.com/reports/api/v2/weekly?user_agent=#{user_agent}&workspace_id=#{workspace_id}")
+    end
+
+    def response(
+      api_key: ENV['TOGGL_API_KEY'],
+      debug: false,
+      uri: self.uri
+    )
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.set_debug_output $stderr if debug
+      request = Net::HTTP::Get.new(uri.request_uri)
+      request.basic_auth api_key, 'api_token'
+      request.add_field 'content-type', 'application/json'
+      http.request(request)
+    end
+
+    def call(response: self.response)
+      return {} unless response.code == '200'
+
+      JSON.parse(response.body)
+    end
+  end
 end
 
-def request_report(
-  api_key: ENV['TOGGL_API_KEY'],
-  debug: false,
-  uri: report_uri
-)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.set_debug_output $stderr if debug
-  request = Net::HTTP::Get.new(uri.request_uri)
-  request.basic_auth api_key, 'api_token'
-  request.add_field 'content-type', 'application/json'
-  http.request(request)
-end
-
-def report_json(response: request_report)
-  return {} unless response.code == '200'
-
-  JSON.parse(response.body)
-end
-
-puts JSON.pretty_generate(report_json)
+puts JSON.pretty_generate(WeeklyReport::Json.call)
